642bc682b63bccc17bcebf70dd85630689c61197
Joseph <47672995+everyonesbuddy@users.noreply.github.com>
2024-06-25 01:16:04 -0400

    add tags for blogs

--- src/app/components/blog-post/blog-post.component.ts
+++ src/app/components/blog-post/blog-post.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BlogService } from 'src/app/services/blog.service';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-blog-post',
@@ -12,7 +13,8 @@ export class BlogPostComponent implements OnInit {
 
   constructor(
     private blogService: BlogService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private metaService: Meta
   ) {}
 
   ngOnInit(): void {
@@ -20,6 +22,26 @@ export class BlogPostComponent implements OnInit {
       let id = params['id'];
       this.blogService.getSingleBlogPost(id).then((blog) => {
         this.blog = blog;
+
+        // Update meta tags based on fetched blog data
+        this.metaService.updateTag({
+          property: 'og:title',
+          content: this.blog.fields.title,
+        });
+        //  this.metaService.updateTag({ property: 'og:description', content: 'Your blog description here' });
+        this.metaService.updateTag({
+          property: 'og:image',
+          content: this.blog.fields.featuredImage.fields.file.url,
+        });
+        this.metaService.updateTag({
+          property: 'og:url',
+          content:
+            'https://sure-odds.com/blog-post/' + this.blog.slug + this.blog.id,
+        });
+
+        // Update other relevant meta tags as needed
+        //  this.metaService.updateTag({ name: 'description', content: this.blog.fields.metaDescription });
+        //  this.metaService.updateTag({ name: 'keywords', content: this.blog.fields.keywords.join(', ') });
       });
     });
   }
