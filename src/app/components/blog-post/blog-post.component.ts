f7af27e8eb27464f85bf4282eda0ffcf5a009654
Joseph <47672995+everyonesbuddy@users.noreply.github.com>
2024-06-27 01:52:54 -0400

    meta tags

--- src/app/components/blog-post/blog-post.component.ts
+++ src/app/components/blog-post/blog-post.component.ts
@@ -23,16 +23,32 @@ export class BlogPostComponent implements OnInit {
       this.blogService.getSingleBlogPost(id).then((blog) => {
         this.blog = blog;
 
-        // Update meta tags dynamically
-        this.meta.updateTag({ property: 'og:title', content: this.blog.title });
+        console.log(this.blog);
+        // Handle title (existing code)
+        let title =
+          this.blog.fields.title.length > 90
+            ? this.blog.fields.title.substring(0, 87) + '...'
+            : this.blog.fields.title;
+        this.meta.updateTag({ property: 'og:title', content: title });
+
+        // Handle image
+        let imageUrl = `https:${this.blog.fields.featuredImage.fields.file.url}`;
+        this.meta.updateTag({ property: 'og:image', content: imageUrl });
+
+        // Handle description
+        let description = this.blog.fields.description;
+        if (!description) {
+          // Extracting summary from content as fallback
+          let contentSummary = this.blog.fields.content.split('\n')[0]; // Taking the first paragraph
+          if (contentSummary.length > 155) {
+            description = contentSummary.substring(0, 152) + '...'; // Truncate if too long
+          } else {
+            description = contentSummary;
+          }
+        }
         this.meta.updateTag({
-          property: 'og:image',
-          content: this.blog.fields.featuredImage.fields.file.url,
-        });
-        this.meta.updateTag({
-          property: 'og:url',
-          content:
-            'https://sure-odds.com/blog-post/' + this.blog.slug + this.blog.id,
+          property: 'og:description',
+          content: description,
         });
       });
     });
